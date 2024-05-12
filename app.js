24fc1f94b2ca2311ba0b66a730957d356b3b2c05
Dania Zakir <zakirdania3@gmail.com>
2024-05-12 20:05:13 +0500

    add app.js and index.html

---
+++ app.js
@@ -0,0 +1,16 @@
+//1. Declare 3 variables in one statement.
+var a=1,b=2,c=3;
+
+//3. Display this in your browser
+
+var sentence="Rules for naming JS variables";
+document.write( sentence+"</br");
+
+var blank="Variable names can only contain;numbers,$ and _. For example : $my_1stVariable";
+document.write(blank+"</br>");
+var blank1="Variables must begin with a letter,$ or _.For example:$name._name or name";
+document.write( "</br>"+blank1 +"</br>");
+var blank2="Variable names are case sensitive";
+document.write(blank2+"</br>");
+var blank3="Variable names should not be JS keywords";
+document.write(blank3);
